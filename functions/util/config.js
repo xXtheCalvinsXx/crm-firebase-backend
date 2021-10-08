ebd361098833e83bc1078e62b1e425baf8cceb7b
andrewzhang616 <andrew.zedy@gmail.com>
2021-10-08 13:39:15 +1100

    added customToken fucntionality

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,3 +1,5 @@
+var serviceAccount = require('./xxthecalvinsxx-firebase-adminsdk-p9n5s-edf52daca8.json');
+const admin = require('firebase-admin');
 module.exports = {
   apiKey: 'AIzaSyC8Tmde2ZCuFnTtZ8HcFLcBckl_fAHs2pQ',
   authDomain: 'xxthecalvinsxx.firebaseapp.com',
@@ -6,4 +8,6 @@ module.exports = {
   messagingSenderId: '499087288068',
   appId: '1:499087288068:web:bff61ab7b5f29a81619251',
   measurementId: 'G-GPSZ6V9868',
+  //serviceAccountId: 'firebase-adminsdk-p9n5s@xxthecalvinsxx.iam.gserviceaccount.com',
+  credential: admin.credential.cert(serviceAccount)
 };
